34ac052628a1cc41b1ffbe5cbb01ebc608ee54f2
jeroen <yeronimuz@gmail.com>
2015-11-13 10:05:51 +0100

    First version of P1 port handler

---
+++ pm-agent/src/main/java/com/lankheet/pmagent/P1Parser.java
@@ -0,0 +1,15 @@
+package com.lankheet.pmagent;
+
+public class P1Parser {
+
+	/**
+	 * Take one complete message and parse it into a P1Datagram object
+	 * @param tempS
+	 * @return
+	 */
+	public static P1Datagram parse(String tempS) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
