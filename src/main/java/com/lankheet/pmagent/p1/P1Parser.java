21bee01059f1e4ea31a9aee75793489fa9116796
Jeroen Lankheet <Jeroen.Lankheet@nexus-nederland.nl>
2018-11-15 22:03:00 +0100

    Replace mit license in every file with a generic one in project root

--- src/main/java/com/lankheet/pmagent/p1/P1Parser.java
+++ src/main/java/com/lankheet/pmagent/p1/P1Parser.java
@@ -1,24 +1,3 @@
-/**
- * MIT License
- * 
- * Copyright (c) 2017 Lankheet Software and System Solutions
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package com.lankheet.pmagent.p1;
 
 import java.time.LocalDateTime;
@@ -73,13 +52,6 @@ public class P1Parser {
 		return p1dg;
 	}
 
-	/**
-	 * Parse a semi-raw value.
-	 * 
-	 * @param p1dg
-	 * @param id
-	 * @param value
-	 */
 	private static void parseP1Line(P1Datagram p1dg, String id, String value) {
 		P1Standard p1 = P1Standard.getDescription(id);
 		LOG.debug("parseP1Line: id = " + id + ", value = " + value);
